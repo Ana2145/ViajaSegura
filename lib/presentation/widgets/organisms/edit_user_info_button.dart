9a3a55e92f710af21a8716f2a9f9a35fb3f28cc4
CrisSoven <20213tn044@utez.edu.mx>
2025-03-31 03:26:33 -0600

    [ADD] diseño y validación genérica de user profile screen

---
+++ lib/presentation/widgets/organisms/edit_user_info_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:viaja_segura_movil/presentation/screens/edit_text_field_page.dart';
+
+class EditUserInfoButton extends StatefulWidget {
+  final String label;
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+
+  const EditUserInfoButton({
+    super.key,
+    required this.label,
+    required this.controller,
+    required this.keyboardType,
+  });
+
+  @override
+  State<EditUserInfoButton> createState() => _EditUserInfoButtonState();
+}
+
+class _EditUserInfoButtonState extends State<EditUserInfoButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: IconButton(
+        icon: const Icon(Icons.edit_outlined),
+        onPressed: () => _editUserInfo(context),
+      ),
+    );
+  }
+
+  Future<void> _editUserInfo(BuildContext context) async {
+    final editedValue = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EditTextFieldPage(
+          label: widget.label,
+          controller: widget.controller,
+          keyboardType: widget.keyboardType,
+        ),
+      ),
+    );
+
+    if (editedValue != null && editedValue is String) {
+      setState(() => widget.controller.text = editedValue);
+    }
+  }
+}
--- lib/presentation/widgets/templates/profile_template.dart
+++ lib/presentation/widgets/templates/profile_template.dart
@@ -13,41 +13,43 @@ class ProfileTemplate extends StatelessWidget {
     final theme = Theme.of(context);
     return Scaffold(
       extendBodyBehindAppBar: true,
-      body: SafeArea(
-        child: Stack(
-          children: [
-            PatternRow(color: theme.primaryColor, height: 4),
-            Column(
-              children: [
-                const SizedBox(height: 32),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const GoBackButton(),
-                          Expanded(
-                            child: Center(
-                              child: Text(
-                                title,
-                                style: theme.textTheme.headlineLarge,
-                                overflow: TextOverflow.ellipsis,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Stack(
+            children: [
+              PatternRow(color: theme.primaryColor, height: 4),
+              Column(
+                children: [
+                  const SizedBox(height: 32),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const GoBackButton(),
+                            Expanded(
+                              child: Center(
+                                child: Text(
+                                  title,
+                                  style: theme.textTheme.headlineLarge,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
                               ),
                             ),
-                          ),
-                          const SizedBox(width: 64),
-                        ],
-                      ),
-                      const SizedBox(height: 64),
-                      child,
-                    ],
+                            const SizedBox(width: 64),
+                          ],
+                        ),
+                        const SizedBox(height: 32),
+                        child,
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            )
-          ],
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
